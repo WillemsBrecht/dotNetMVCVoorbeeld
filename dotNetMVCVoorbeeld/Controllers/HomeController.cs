53abe6c7f18c934db3f8684c1c2c0c3a1f25ab24
WillemsBrecht <brecht.willems2@student.howest.be>
2018-02-19 11:30:21 +0100

    blablz

---
+++ dotNetMVCVoorbeeld/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotNetMVCVoorbeeld.Controllers {
+    public class HomeController : Controller {
+        public string Index(string name) {
+            return $"Wazz Gud {name}.";
+        }
+    }
+}
