676554815a34da86744664929672f04cc7e4761b
WillemsBrecht <brecht.willems2@student.howest.be>
2018-02-19 11:56:24 +0100

    added zoek groenten

--- dotNetMVCVoorbeeld/Controllers/HomeController.cs
+++ dotNetMVCVoorbeeld/Controllers/HomeController.cs
@@ -6,8 +6,21 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace dotNetMVCVoorbeeld.Controllers {
     public class HomeController : Controller {
-        public string Index(string name) {
-            return $"Wazz Gud {name}.";
+
+        private string[] groenten = { "Rode kool", "Wortel", "Aardappel", "Bloemkool" };
+        public ViewResult Index(string name) {
+            ViewBag.Groet = DateTime.Now.Hour < 12 ? "Goeiemorgen" : "Goeimiddag";
+            return View();
+        }
+
+        public ViewResult Groenten(string zoek) {
+
+            ViewBag.Groenten = groenten;
+            if (!string.IsNullOrEmpty(zoek)) {
+                ViewBag.Zoekresultaat = $"De gezochte groente is " +
+                                        $"{Array.IndexOf(groenten, zoek) + 1}e uit de lijst van groenten";
+            }
+            return View();
         }
     }
 }
